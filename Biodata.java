9878604985c15c5e5b688900abc1ed2f26928b7b
Rikenro <rikenroaldifo@gmail.com>
2024-09-11 05:17:27 +0700

    Create Biodata.java

---
+++ Biodata.java
@@ -0,0 +1,12 @@
+public class Biodata {
+    public static void main(String[] args) {
+        String nama = "Rikenro Aldifo";
+        String NIM = "12350112981";
+        String hobi = "Membaca buku";
+
+        System.out.println("Nama :" + nama + ".");
+        System.out.println("NIM :" + NIM);
+        System.out.println("Hobi :" + hobi + ".");
+    }
+}
+
